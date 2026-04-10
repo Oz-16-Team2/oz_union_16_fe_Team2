b0a452f722c9e9b925967a30427ce2773276cf7e
ohseungyeon <alstmd9902@naver.com>
2026-04-11 02:33:03 +0900

    refactor: apply barrel exports and update import paths (#18)

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-// components 폴더의 공통 export를 한곳에서 관리
-
-export { default as Footer } from './layouts/Footer'
-export { default as Header } from './layouts/Header'
-export { default as RootLayout } from './layouts/RootLayout'
