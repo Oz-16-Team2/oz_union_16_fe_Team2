d1d7159ae1f2cb68abfa1d86548d5d95c970a53b
0rrriiinnn0 <0rrriiinnn0@naver.com>
2026-04-13 02:17:32 +0900

    refactor: apply tailwind utility classes to badge (#15)

--- src/components/common/badge/Badge.tsx
+++ src/components/common/badge/Badge.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/utils/cn'
 
 const badgeVariants = cva(
-  'inline-flex items-center justify-center min-w-[68px] rounded-[9999px] px-[18px] py-[8px] text-[12px] font-semibold whitespace-nowrap',
+  'inline-flex items-center justify-center min-w-[68px] rounded-full px-[18px] py-[8px] text-xs font-semibold whitespace-nowrap',
   {
     variants: {
       variant: {
