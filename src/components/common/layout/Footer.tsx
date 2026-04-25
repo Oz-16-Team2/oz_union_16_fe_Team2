1030a67e1e7d55ce7b0fc3b75c1b6348ae7217cb
ohseungyeon <alstmd9902@naver.com>
2026-04-25 11:49:01 +0900

    style: refine layout, modal portal, and custom scrollbars (#116)

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -9,7 +9,7 @@ import { FOOTER_GITHUB_LINKS } from './constants/footer'
 
 export function Footer() {
   return (
-    <footer className="bg-gray-950 py-14 border-t border-t-[#202020]">
+    <footer className="bg-gray-950 py-10 border-t border-t-[#202020]">
       <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4">
         <img src={DarklogoImage} className="w-14" alt="OZ Union 로고" />
         <div className="flex flex-col gap-2">
