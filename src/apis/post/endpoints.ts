f79e0a17758102c30635d9ddf4625b05f3ff7365
hyunsik2000 <ryuhsik2000@naver.com>
2026-04-29 14:53:50 +0900

    feat: add post api endpoint and ranking (#118)

--- src/apis/post/endpoints.ts
+++ src/apis/post/endpoints.ts
@@ -1,10 +1,13 @@
 export const POST_ENDPOINTS = {
-  goals: '/goals',
-  tags: '/tags',
-  posts: '/posts',
-
-  post: (postId: number | string) => `/posts/${postId}/`,
+  goals: '/goals/',
+  tags: '/posts/tags',
+  post: (postId: number) => `/posts/${postId}/`,
+  posts: '/posts/',
   postLikes: (postId: number | string) => `/posts/${postId}/likes/`,
   postScraps: (postId: number | string) => `/posts/${postId}/scraps`,
   postReports: (postId: number | string) => `/posts/${postId}/reports/`,
+  presignedUrl: '/posts/presigned-url/',
+  postSearch: '/posts/search',
+  postTrending: '/posts/trending',
+  postSuggestions: '/posts/suggestions',
 } as const
