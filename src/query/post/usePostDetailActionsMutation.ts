8ab1162b244d3a4caf13562c3c920e7a06fbd371
0rrriiinnn0 <0rrriiinnn0@naver.com>
2026-05-01 19:48:59 +0900

    chore: continue post detail actions work (#138)

--- src/query/post/usePostDetailActionsMutation.ts
+++ src/query/post/usePostDetailActionsMutation.ts
@@ -33,25 +33,9 @@ export function useTogglePostScrapMutation({
   })
 }
 
-<<<<<<< HEAD
-// TODO: 포스트 디테일 페이지에서 신고가 필요할 때는 src/query/post에서 useReportPostMutation을 import해서 사용하면 됩니다.
-
-// export function useReportPostMutation(postId: number) {
-//   return useMutation({
-//     mutationFn: (body: { reason_type: string; reason_detail: string }) =>
-//       postApi.reportPost(postId, body),
-//   })
-// }
-=======
-export function useDeletePostMutation(postId: number) {
-  const queryClient = useQueryClient()
-
+export function useReportPostMutation(postId: number) {
   return useMutation({
-    mutationFn: () => postApi.deletePost(postId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['posts'] })
-      queryClient.invalidateQueries({ queryKey: ['postDetail', postId] })
-    },
+    mutationFn: (body: { reason_type: string; reason_detail: string }) =>
+      postApi.reportPost(postId, body),
   })
 }
->>>>>>> 84abc54 (feat: connect post detail api (#138))
