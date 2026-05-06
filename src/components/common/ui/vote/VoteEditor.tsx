77b107418bead8ea453e123ef390be3a9d356e40
0rrriiinnn0 <0rrriiinnn0@naver.com>
2026-05-06 16:57:54 +0900

    fix: polish post detail dark mode UI (#177)

--- src/components/common/ui/vote/VoteEditor.tsx
+++ src/components/common/ui/vote/VoteEditor.tsx
@@ -14,10 +14,9 @@ const inputVariants = {
 
   enabled: {
     first:
-      'border border-gray-300 bg-gray-300 text-text-primary dark:border-white/15 dark:bg-white/20',
-
+      'border border-gray-300 bg-gray-300 text-text-primary dark:border-white/15 dark:bg-white/10',
     second:
-      'border border-primary-100 bg-primary-100 text-text-primary dark:border-primary-400 dark:bg-primary-400',
+      'border border-primary-100 bg-primary-100 text-text-primary dark:border-primary-400/30 dark:bg-primary-600/20',
   },
 
   disabled:
